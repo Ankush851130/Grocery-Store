036e2ab6c1b0fe409f88bddb5fdc121868adc7a5
Ankush Kumar <ankushvivo2020@gmail.com>
2026-07-23 09:33:37 +0530

    udpate

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,12 +1,18 @@
 const mongoose = require('mongoose');
 
+let isConnected = false;
+
 const connectDatabase = async () => {
+  if (isConnected || mongoose.connection.readyState >= 1) {
+    return;
+  }
+
   if (!process.env.MONGODB_URI) {
     throw new Error('MONGODB_URI is not defined in environment variables');
   }
 
-  await mongoose.connect(process.env.MONGODB_URI);
-
+  const db = await mongoose.connect(process.env.MONGODB_URI);
+  isConnected = db.connections[0].readyState;
   console.log('MongoDB connected successfully');
 };
 
